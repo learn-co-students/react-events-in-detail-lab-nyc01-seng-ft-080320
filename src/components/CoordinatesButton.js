e92b5e491ac8cb690af1846148c63ec086e6103c
kainan54 <kaindoneson@gmail.com>
2020-10-05 16:56:54 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends Component {
+
+    bubbaCords = e => {
+        this.props.onReceiveCoordinates([e.clientX, e.clientY]);
+    }
+
+    render() {
+        return(
+            <button
+                onClick = {this.bubbaCords}
+            ></button>
+        )
+    }
+}
+
+export default CoordinatesButton
